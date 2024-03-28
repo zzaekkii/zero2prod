11e6fc59ccbe8e1dffd390833f08ceafa06ff783
zakie <realzzaekkii@gmail.com>
2024-03-28 18:43:30 +0900

    :tada: feat: health_check endpoint(진짜) 이게 진짜고 이전 커밋은 테스트

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok() // .finish() 생략 가능.
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+        App::new().route("/health_check", web::get().to(health_check)) // route 경유해 App에 등록.
     })
     .bind("127.0.0.1:8000")?
     .run()
