6de7086117d9bd8b135af787b08ccb2b1c6449f7
zakie <realzzaekkii@gmail.com>
2024-04-01 10:41:50 +0900

    :white_check_mark: test: 헬스_체크 GET 요청 200 OK 응답 테스트 추가

---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+#[tokio::test] // 테스팅에선 기능적으로 tokio::main과 동등.
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed to spawn our app.");
+
+    // 'reqwest'를 이용해 application에 대한 http 요청 수행.
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// 백그라운드에서 application 구동.
+async fn spawn_app() -> std::io::Result<()> {
+    todo!()
+}
