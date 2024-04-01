052f62143cc21c82b6487cc6bb98f094589f4814
zakie <realzzaekkii@gmail.com>
2024-04-01 09:42:42 +0900

    :recycle: refact: 기존 코드 라이브러리 및 바이너리로 리팩토링

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok() // .finish() 생략 가능.
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check)) // route 경유해 App에 등록.
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
